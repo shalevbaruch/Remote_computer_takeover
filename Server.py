71e2e055ccf4f03dc0fe5ae6d5153d03d84cfe1f
shalevbaruch <100311710+shalevbaruch@users.noreply.github.com>
2023-03-17 14:16:04 +0200

    Update Server.py
    checking keyboard

--- Server.py
+++ Server.py
@@ -1,8 +1,12 @@
 import sys
-sys.path.append("C:/University/YoungForTech/networks")
-import keyboard
+sys.path.append("C:/University")  # This is the path on my desktop computer
+sys.path.append("C:/University/YoungForTech/networks")  #Thie is the path on my laptop
 import os
 import threading
+import keyboard
+import time
+import socket
+import ssl
 
 try:
     from Sending_Files_System.server import My_Server
@@ -26,16 +30,44 @@ def handleScreenshot(server, ssl_client_soc):
     print("finished")
 
 
-def sendKeys(keysServer, keysSock):
-    message = keysSock.recv(20).decode()
-    print(message)
+def sendKeys(keysSock):
     while True:
         try: 
             key = keyboard.read_event()
+            print(key.event_type)
             if key.event_type == 'down':
                 keysSock.send(key.name.encode())
         except:
             break
+
+
+def sendkeys2(sock):
+    while True:
+        try:
+            event = keyboard.read_event()
+            # check if the event is a key down event
+            if event.event_type == "down":
+                # get the key name, including special keys like ctrl+z
+                key_name = keyboard.key_to_scan_codes(event.name)[0]
+                # send the key name to the server
+                message = f"{key_name}".encode()
+                sock.sendall(message)
+        except KeyboardInterrupt:
+            # close the socket and exit the loop
+            sock.close()
+            break
+
+
+def connect_My_Server(Server_IP, Server_Port, Transport_Layer_Protocol):
+    server_address = (Server_IP, Server_Port)
+    if Transport_Layer_Protocol == "UDP":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    else:  # Transport_Layer_Protocol == "TCP":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_sock = ssl.wrap_socket(sock)  # add a security layer
+        ssl_sock.connect(server_address)     
+    return ssl_sock
 
 
 
@@ -43,13 +75,19 @@ def sendKeys(keysServer, keysSock):
 
 
 if __name__ == '__main__':
-    screenshotServer = My_Server(LISTEN_PORT=9124, SIMULTANEOUS_REQUESTS_LIMIT=1,TRANSPORT_LAYER_PROTOCOL="TCP", HANDLE=handleScreenshot)
-    t1 = threading.Thread(target=screenshotServer.start)
-    t1.start()
-    t1.join()
+    # screenshotServer = My_Server(LISTEN_PORT=9124, SIMULTANEOUS_REQUESTS_LIMIT=1,TRANSPORT_LAYER_PROTOCOL="TCP", HANDLE=handleScreenshot)
+    # t1 = threading.Thread(target=screenshotServer.start)
+    # t1.start()
+    # t1.join()
 
-    # keyboardServer = My_Server(LISTEN_PORT=9200, SIMULTANEOUS_REQUESTS_LIMIT=2,TRANSPORT_LAYER_PROTOCOL="TCP", HANDLE=sendKeys)
-    # keyboardServer.start()
+
+    # keyboard_server_ip = "127.0.0.1"
+    keyboard_server_ip =  "10.0.0.60"  # when I'm using my laptop as the customer and I'm at home (The IP will probable change if I am at Tel-Aviv)
+    
+    keyboard_server_port = 9200
+    Transport_Layer_Protocol = "TCP"
+    keyboard_sock = connect_My_Server(keyboard_server_ip, keyboard_server_port, Transport_Layer_Protocol)
+    sendKeys(keyboard_sock)
     # t2 = threading.Thread(target=keyboardServer.start)
     # t2.start()
     # t2.join()
